779ed407d7e7e4b98a365bf420ca80b25028276e
Eliana-Abate <eliana.abate@outlook.it>
2021-10-11 17:16:25 +0200

    added model Comic

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Comic;
+use Illuminate\Http\Request;
+
 
 class ComicController extends Controller
 {
@@ -14,7 +15,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        $comics = [];
+        $comics = Comic::all();
         return view('comics.index', compact('comics'));
         
     }
