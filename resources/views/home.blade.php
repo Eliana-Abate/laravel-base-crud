e7a43b3d3789d4401f79372e04c8c434b6f25647
Eliana-Abate <eliana.abate@outlook.it>
2021-10-11 16:58:37 +0200

    added text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,11 @@
+@extends('layouts.main')
 
+@section('title') Home @endsection
 
+@section('content')
+    <div class="container text-center">
+        <h1>Go to <a href="{{route('comics.index')}}">Comics</a></h1>
+
+    </div>
+    
+@endsection
