63e143d5f1deed34bb59d9325c2f2b5647becb58
Eliana-Abate <eliana.abate@outlook.it>
2021-10-11 19:43:14 +0200

    edited function show and used dependency injection (parameter Comic $comic)

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -47,9 +47,8 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comic)
     {
-        $comic = Comic::findOrFail($id);
         return view('comics.show', compact('comic'));
     }
 
