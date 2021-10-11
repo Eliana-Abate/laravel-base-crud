b6821fab66e2d8a5b7efa59e2c3f3f5558ff22d0
Eliana-Abate <eliana.abate@outlook.it>
2021-10-11 17:02:58 +0200

    added table structure

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('title') Comics @endsection
+
+@section('content') 
+    <section id="comics-list">
+        <div class="container">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Cover</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Sale Date</th>
+                    <th scope="col">Type</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row"></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+
+        </div>
+
+    </section>
+@endsection
