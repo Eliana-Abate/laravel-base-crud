f974032902a00540bd2b5b6f7b05cc079e24d8d4
Eliana-Abate <eliana.abate@outlook.it>
2021-10-13 15:55:12 +0200

    added label description and style

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,38 +2,68 @@
 
 @section('content') 
 
-<form>
-    <div class="mb-3">
-      <label for="" class="form-label"></label>
-      <input type="" class="form-control" id="">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label"></label>
-        <input type="" class="form-control" id="">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label"></label>
-        <input type="" class="form-control" id="">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label"></label>
-        <input type="" class="form-control" id="">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label"></label>
-        <input type="" class="form-control" id="">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label"></label>
-        <input type="" class="form-control" id="">
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label"></label>
-        <input type="" class="form-control" id="">
+<section id="form-create" class="pt-5">
+    <h1 class="text-white text-center pb-5">Aggiungi un nuovo fumetto</h1>
+    <div class="container">
+        <div class="row border border-5 border-primary bg-white p-5">
+            <form>
+                <div class="row">
+                    <div class="col-6 mb-3">
+                        <label for="" class="form-label">Titolo</label>
+                        <input type="text" class="form-control" id="">
+                    </div>
+
+                    <div class="col-6 mb-3">
+                          <label for="" class="form-label">Serie</label>
+                          <input type="text" class="form-control" id="">
+                    </div>
+                </div>
+                
+                <div class="mb-3">
+                    
+                    <label for="" class="form-label">Descrizione</label>
+                    <textarea class="form-control"  id="" style="height: 100px"></textarea>
+
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="form-label">Url copertina</label>
+                    <input type="text" class="form-control" id="">
+                </div>
+
+                <div class="row">
+                    <div class="col-4 mb-3">
+                        <label for="" class="form-label">Prezzo</label>
+                        <input type="text" class="form-control" id="">
+                    </div>
+
+                    <div class="col-4 mb-3">
+                        <label for="" class="form-label">Data di uscita</label>
+                        <input type="text" class="form-control" id="">
+                    </div>
+
+                    <div class="col-4 mb-3">
+                        <label for="" class="form-label">Tipologia</label>
+                        <input type="text" class="form-control" id="">
+                    </div>
+
+                </div>
+
+                
+                
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            
+
+        </div>
+
+       
     </div>
     
-    
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+</section>
+
+
 
 @endsection
