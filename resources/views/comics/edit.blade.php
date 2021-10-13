79ce5505af60b88c3300c9163b0782412616dc55
Eliana-Abate <eliana.abate@outlook.it>
2021-10-13 17:17:13 +0200

    added form in edit.blade

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.main')
+
+@section('content') 
+
+<section id="form-create" class="pt-5">
+    <h1 class="text-white text-center pb-5">Modifica fumetto</h1>
+    <div class="container pb-5">
+        <div class="row border border-5 border-primary bg-white p-5">
+            <form method="POST" action="{{route('comics.update', $comic->id)}}">
+                @method('PATCH')
+                @csrf
+
+                <div class="row">
+                    <div class="col-6 mb-3">
+                        <label for="title" class="form-label">Titolo</label>
+                        <input type="text" class="form-control" id="title" name="title" required>
+                    </div>
+
+                    <div class="col-6 mb-3">
+                          <label for="series" class="form-label">Serie</label>
+                          <input type="text" class="form-control" id="series" name="series" required>
+                    </div>
+                </div>
+                
+                <div class="mb-3">
+                    
+                    <label for="description" class="form-label">Descrizione</label>
+                    <textarea class="form-control"  id="description" name="description" style="height: 100px"></textarea>
+
+                </div>
+
+                <div class="mb-3">
+                    <label for="thumb" class="form-label">Url copertina</label>
+                    <input type="text" class="form-control" id="thumb" name="thumb">
+                </div>
+
+                <div class="row mb-5">
+                    <div class="col-4 mb-3">
+                        <label for="price" class="form-label">Prezzo</label>
+                        <input type="text" class="form-control" id="price" name="price" required>
+                    </div>
+
+                    <div class="col-4 mb-3">
+                        <label for="sale_date" class="form-label">Data di uscita</label>
+                        <input type="text" class="form-control" id="sale_date" name="sale_date" required>
+                    </div>
+
+                    <div class="col-4 mb-3">
+                        <label for="type" class="form-label">Tipologia</label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    <button type="reset" class="btn btn-secondary me-3">Reset</button>
+                    <button type="submit" class="btn btn-primary">Salva modifiche</button>
+                </div> 
+            </form>
+        </div>
+    </div>
+</section>
+
+
+@endsection
