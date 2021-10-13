763cd876bad4858bc07569f6a69a6d7b5cbecc94
Eliana-Abate <eliana.abate@outlook.it>
2021-10-13 15:37:46 +0200

    added form (empty basic structure)

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('content') 
+
+<form>
+    <div class="mb-3">
+      <label for="" class="form-label"></label>
+      <input type="" class="form-control" id="">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label"></label>
+        <input type="" class="form-control" id="">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label"></label>
+        <input type="" class="form-control" id="">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label"></label>
+        <input type="" class="form-control" id="">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label"></label>
+        <input type="" class="form-control" id="">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label"></label>
+        <input type="" class="form-control" id="">
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label"></label>
+        <input type="" class="form-control" id="">
+    </div>
+    
+    
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+@endsection
