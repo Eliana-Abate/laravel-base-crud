dd4c597cc0b41905bd8ca94a1464bee056a8b74a
Eliana-Abate <eliana.abate@outlook.it>
2021-10-13 20:33:29 +0200

    added superman and skyline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,16 @@
 @section('title') Home @endsection
 
 @section('content')
-    <div class="container vh-100 d-flex align-items-center justify-content-center text-white">
-        <h1>Go to <a href="{{route('comics.index')}}">Comics</a> ...</h1>
+<section id="home-container">
+    <div class="container vh-100 d-flex flex-column align-items-center justify-content-center text-white">
+        <h3>Fly to</h3>
+        <h1><a class="text-warning" href="{{route('comics.index')}}">Comics</a></h1>
+        <div class="img-flight-box superman-flight">
+            <div class="img-bounce-box bounce">
+                <img  src="{{asset('images/superman.png')}}" alt="superman">
+            </div>  
+        </div>  
     </div>
+</section>
     
 @endsection
