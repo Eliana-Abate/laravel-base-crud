35856fa7f0538a3ee0c71f97c5971b2f90ba946d
Eliana-Abate <eliana.abate@outlook.it>
2021-10-13 17:30:43 +0200

    filled and tested function update

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -64,6 +64,7 @@ class ComicController extends Controller
     public function edit(Comic $comic)
     {
         return view('comics.edit', compact('comic'));
+    
     }
 
     /**
@@ -73,9 +74,11 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->all();
+        $comic->update($data);
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
